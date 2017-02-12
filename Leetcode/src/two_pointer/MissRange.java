af58aa399b128286cb65149a0bc5e3ae94001c59
TeslaZ <tluo4@stevens.edu>
2017-02-12 18:22:53 -0500

    missing range

---
+++ Leetcode/src/two_pointer/MissRange.java
@@ -0,0 +1,41 @@
+package two_pointer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MissRange {
+	public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+		List<String> result = new ArrayList<>();
+		int prev = lower;
+		for (int i = 0; i <= nums.length; i++) {
+			int cur = 0;
+			if (i != nums.length && nums[i] <= upper){
+				cur = nums[i];
+			}else{
+				cur = upper+1;
+			}
+			if (prev == cur) {
+				prev++;
+			}else if (prev < cur){
+				if (cur - prev == 1){
+					result.add(String.valueOf(prev));
+				}else{
+					result.add(String.valueOf(prev)+"->"+String.valueOf(cur-1));
+				}
+				prev = cur+1;
+			}else{
+				continue;
+			}
+		}
+		return result;
+	}
+	// test case 1: lower = 0, upper = 99 and Given array [0,1,3,50,75]
+	// the output should be ["2","4->49","51->74","76->99"]
+	@Test
+	public void test() {
+		int[] nums = {0,1,3,50,75};
+		System.out.println(findMissingRanges(nums, 10, 50));
+	}
+}
